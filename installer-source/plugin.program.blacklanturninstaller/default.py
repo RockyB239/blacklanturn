c17ab3086c5cf9eaf1b1fc268de969068f0d707d
RockyB239 <rockyb239@users.noreply.github.com>
2026-05-15 21:32:19 -0400

    Fix installer dialog API call

--- installer-source/plugin.program.blacklanturninstaller/default.py
+++ installer-source/plugin.program.blacklanturninstaller/default.py
@@ -2,6 +2,5 @@ import xbmcgui
 
 xbmcgui.Dialog().ok(
     "BlackLanturn Installer",
-    "Installer skeleton is installed correctly.",
-    "OpenWizard integration comes next."
+    "Installer skeleton is installed correctly.\n\nOpenWizard integration comes next."
 )
